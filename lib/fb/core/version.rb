2e0c49abd0b5461b1d358f00ecd06049220abfc6
Kang-Kyu Lee <kangkyu1111@gmail.com>
2018-02-14 16:17:02 -0800

    Bump the version to beta

--- lib/fb/core/version.rb
+++ lib/fb/core/version.rb
@@ -3,6 +3,6 @@ module Fb
   class Core
     # @return [String] the SemVer-compatible gem version.
     # @see http://semver.org
-    VERSION = '1.0.0.alpha12'
+    VERSION = '1.0.0.beta1'
   end
 end
