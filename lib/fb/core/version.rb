1c035de611d66182647182c89679e1ca5f7952f0
Kang-Kyu Lee <kangkyu1111@gmail.com>
2018-04-30 18:27:59 -0700

    Add time range options to page videos

--- lib/fb/core/version.rb
+++ lib/fb/core/version.rb
@@ -3,6 +3,6 @@ module Fb
   class Core
     # @return [String] the SemVer-compatible gem version.
     # @see http://semver.org
-    VERSION = '1.0.0.beta9'
+    VERSION = '1.0.0.beta10'
   end
 end
